ac569617d4b69b17ed0a400027651c8d4306d7bf
Andrea Serafini <andrea.serafini11@studio.unibo.it>
2021-03-08 11:10:49 +0100

    Moved source code to src folder

--- src/u02/home/Tree.scala
+++ src/u02/home/Tree.scala
@@ -3,16 +3,18 @@ package u02.home
 object Tree {
 
   sealed trait Tree[A]
+
   case class Branch[A](left: Tree[A], elem: A, right: Tree[A]) extends Tree[A]
+
   case class Empty[A]() extends Tree[A]
 
-  def sum(tree: Tree[Int]): Int = tree match{
+  def sum(tree: Tree[Int]): Int = tree match {
     case Branch(left, elem, right) => sum(left) + elem + sum(right)
     case Empty() => 0
   }
 
   def count[A](tree: Tree[A]): Int = tree match {
-    case Branch(left,elem,right) => count(left) + 1 + count(right)
+    case Branch(left, elem, right) => count(left) + 1 + count(right)
     case Empty() => 0
   }
 }
